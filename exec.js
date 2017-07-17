acef7224f1942d8b311a7fd65f2401323d70faf8
optimalisatie <info@optimalisatie.nl>
2017-07-17 17:11:13 +0200

    modified:   bower.json 	modified:   exec.js 	modified:   exec.min.js 	modified:   package.json 	modified:   tests/webworker-vs-execjs-ping.html

--- exec.js
+++ exec.js
@@ -1,55 +1,33 @@
 /**
  * Cancellable Javascript Code Runner
- * @version 1.0.4
+ * @version 1.0.5
  * @link https://github.com/optimalisatie/exec.js
  */
 (function(window) {
 
     // container
-    var document = window.document,
-        c, f = document.createElement('iframe');
+    var c, f = document.createElement('iframe');
 
     // constructor
     var exec = function(code, callback) {
+
         var runner = this,
             id = "_" + +new Date() + Math.random().toFixed(16).substring(2),
             e = 'EventListener',
             E = 'Event',
             p = 'postMessage',
-            o = 'onmessage';
+            o = 'onmessage',
+            s, i, started, stopped, q = [];
 
         // event handler
         e = window['add' + e] ? ['add' + e, 'remove' + e] : ['attach' + E, 'detach' + E];
         E = ((e[0] === 'attach' + E) ? 'on' : '') + 'message';
 
-        // create code execution container
-        if (!c) {
-            c = document.createElement('div');
-            c.style.display = 'none';
-            document.body.appendChild(c);
-        }
-        var i = f.cloneNode(false);
-        i.name = id;
-        i.style.display = 'none';
-        c.appendChild(i);
-        var d = (i.contentWindow || i.contentDocument);
-        if (d.document) {
-            d = d.document
-        }
-
-        // process data
-        var P = function(m) {
-            if (m.data[0] === id && callback) {
-                callback(m.data[1]);
-            }
-        }
-
         // stop code execution
-        var stopped;
-        this.stop = function() {
+        this.s = function() {
 
             // stopped
-            if (stopped) {
+            if (stopped || !started) {
                 return;
             }
             stopped = true;
@@ -74,9 +52,20 @@
             window[e[1]](E, P, false);
         };
 
+        // process data
+        var P = function(m) {
+            if (m.data[0] === id && callback) {
+                callback(m.data[1]);
+            }
+        }
+
         // post data to container
-        var s;
-        this.post = function(data, transferableList) {
+        this.p = function(data, transferableList) {
+
+            // wait for start
+            if (!started) {
+                return q.push([data, transferableList]);
+            }
 
             if (!s) {
                 s = i.contentWindow[p].bind(i.contentWindow);
@@ -89,26 +78,63 @@
             s(data, "*", transferableList);
         };
 
-        // watch message event
-        window[e[0]](E, P, false);
+        // start
+        var start = function() {
 
-        // convert to IIFE
-        code = '(' + ((typeof code === 'function') ? code.toString() : 'function(' + p + '){' + code + '}') + ')(' + p + ')';
+            // wait for body
+            if (!document.body) {
+                return setTimeout(start, 0);
+            }
+            started = true;
 
-        // execute code
-        d.open();
-        d.write('<script>(function() {var ' + o + ';var P=function(){if(' + o + '){P = ' + o + ';}' + o + '.apply(this,arguments);};var ' + p + '=function(d,t){parent.' + p + '(["' + id + '",d],"*",t);};' + code + ';window["' + e[0] + '"]("' + E + '",P,false);})();</script>');
-        d.close();
+            // create code execution container
+            if (!c) {
+                c = document.createElement('div');
+                c.style.display = 'none';
+                document.body.appendChild(c);
+            }
+            i = f.cloneNode(false);
+            i.name = id;
+            i.style.display = 'none';
+            c.appendChild(i);
+            var d = (i.contentWindow || i.contentDocument);
+            if (d.document) {
+                d = d.document
+            }
+
+            // watch message event
+            window[e[0]](E, P, false);
+
+            // convert to IIFE
+            code = '(' + ((typeof code === 'function') ? code.toString() : 'function(' + p + '){' + code + '}') + ')(' + p + ')';
+
+            // execute code
+            d.open();
+            d.write('<script>(function() {var ' + o + ';var P=function(){if(' + o + '){P = ' + o + ';}' + o + '.apply(this,arguments);};var ' + p + '=function(d,t){parent.' + p + '(["' + id + '",d],"*",t);};' + code + ';window["' + e[0] + '"]("' + E + '",P,false);})();</scr' + 'ipt>');
+            d.close();
+
+            // process queued posts
+            if (q) {
+                var Q;
+                while (Q = q.shift()) {
+                    runner.post.apply(this, Q);
+                }
+            }
+
+        };
+
+        // start
+        start();
     };
 
     // post data to container
     exec.prototype.post = function(data, transferableList) {
-        this.post(data, transferableList);
+        this.p(data, transferableList);
     };
 
     // abort execution
     exec.prototype.stop = function() {
-        this.stop();
+        this.s();
     };
 
     // define public constructor
