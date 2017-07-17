5b5c97e25a2bdd76533ec09a0f0d9d3ac6639821
optimalisatie <info@optimalisatie.nl>
2017-07-17 15:12:44 +0200

    modified:   bower.json 	modified:   exec.js 	modified:   exec.min.js 	modified:   package.json

--- exec.js
+++ exec.js
@@ -3,94 +3,109 @@
  * @version 1.0.2
  * @link https://github.com/optimalisatie/exec.js
  */
-var exec = function(code, callback) {
-    var runner = this,
-        id = "_" + +new Date() + Math.random().toFixed(16).substring(2),
-        e = 'EventListener',
-        E = 'Event',
-        p = 'postMessage',
-        o = 'onmessage';
+var exec = (function(window) {
 
-    // event handler
-    e = window['add' + e] ? ['add' + e, 'remove' + e] : ['attach' + E, 'detach' + E];
-    E = ((e[0] === 'attach' + E) ? 'on' : '') + 'message';
+    // container
+    var document = window.document,
+        c, f = document.createElement('iframe');
 
-    // create code execution container
-    var i = document.createElement('iframe');
-    i.name = id;
-    i.style = 'display:none;';
-    document.body.appendChild(i);
-    var d = (i.contentWindow || i.contentDocument);
-    if (d.document) {
-        d = d.document
-    }
+    // constructor
+    var exec = function(code, callback) {
+        var runner = this,
+            id = "_" + +new Date() + Math.random().toFixed(16).substring(2),
+            e = 'EventListener',
+            E = 'Event',
+            p = 'postMessage',
+            o = 'onmessage';
 
-    // process data
-    var processData = function(m) {
-        if (m.data[0] === id && callback) {
-            callback(m.data[1]);
+        // event handler
+        e = window['add' + e] ? ['add' + e, 'remove' + e] : ['attach' + E, 'detach' + E];
+        E = ((e[0] === 'attach' + E) ? 'on' : '') + 'message';
+
+        // create code execution container
+        if (!c) {
+            c = document.createElement('div');
+            c.style.display = 'none';
+            document.body.appendChild(c);
         }
-    }
-
-    // stop code execution
-    var stopped;
-    this.stop = function() {
-
-        // stopped
-        if (stopped) {
-            return;
+        var i = f.cloneNode(false);
+        i.name = id;
+        i.style.display = 'none';
+        c.appendChild(i);
+        var d = (i.contentWindow || i.contentDocument);
+        if (d.document) {
+            d = d.document
         }
-        stopped = true;
 
-        try {
-            var frm = window.frames[id];
-
-            // IE
-            if (typeof frm.stop === 'undefined') {
-                frm.document.execCommand('Stop');
-            } else {
-                frm.stop();
+        // process data
+        var P = function(m) {
+            if (m.data[0] === id && callback) {
+                callback(m.data[1]);
             }
-        } catch (e) {}
+        }
 
-        // remove from document
-        try {
-            document.body.removeChild(i);
-        } catch (e) {}
+        // stop code execution
+        var stopped;
+        this.stop = function() {
 
-        // remove listener
-        window[e[1]](E, processData, false);
+            // stopped
+            if (stopped) {
+                return;
+            }
+            stopped = true;
+
+            try {
+                var frm = window.frames[id];
+
+                // IE
+                if (typeof frm.stop === 'undefined') {
+                    frm.document.execCommand('Stop');
+                } else {
+                    frm.stop();
+                }
+            } catch (e) {}
+
+            // remove from document
+            try {
+                c.removeChild(i);
+            } catch (e) {}
+
+            // remove listener
+            window[e[1]](E, P, false);
+        };
+
+        // post data to container
+        this.post = function(data, transferableList) {
+
+            // stopped
+            if (stopped) {
+                return;
+            }
+
+            i.contentWindow[p](data, document.location.href, transferableList);
+        };
+
+        // watch message event
+        window[e[0]](E, P, false);
+
+        // convert to IIFE
+        code = '(' + ((typeof code === 'function') ? code.toString() : 'function(' + p + '){' + code + '}') + ')(' + p + ')';
+
+        // execute code
+        d.open();
+        d.write('<script>(function() {var ' + o + ';var P=function(){if(' + o + '){' + o + '.apply(this,arguments);}};var ' + p + '=function(d,t){parent.' + p + '([\'' + id + '\',d],' + JSON.stringify(document.location.href) + ',t);};' + code + ';window[\'' + e[0] + '\'](\'' + E + '\',P,false);})();</script>');
+        d.close();
     };
 
     // post data to container
-    this.post = function(data, transferableList) {
-
-        // stopped
-        if (stopped) {
-            return;
-        }
-
-        i.contentWindow[p](data, document.location.href, transferableList);
+    exec.prototype.post = function(data, transferableList) {
+        this.post(data, transferableList);
     };
 
-    // watch message event
-    window[e[0]](E, processData, false);
+    // abort execution
+    exec.prototype.stop = function() {
+        this.stop();
+    };
 
-    // convert to IIFE
-    code = '(' + ((typeof code === 'function') ? code.toString() : 'function(' + p + '){' + code + '}') + ')(' + p + ')';
-
-    // execute code
-    d.open();
-    d.write('<script>(function() {var ' + o + ';var P=function(){if(' + o + '){' + o + '.apply(this,arguments);}};var ' + p + '=function(d,t){parent.' + p + '([\'' + id + '\',d],' + JSON.stringify(document.location.href) + ',t);};' + code + ';window[\'' + e[0] + '\'](\'' + E + '\',P,false);})();</script>');
-    d.close();
-};
-
-// post data to container
-exec.prototype.post = function(data, transferableList) {
-    return this.post(data, transferableList);
-};
-
-// abort execution
-exec.prototype.stop = function() {
-    return this.stop();
-};
+    return exec;
+})(window);
