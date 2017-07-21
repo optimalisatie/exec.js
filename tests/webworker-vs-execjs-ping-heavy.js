046878fdfaedef0de7cb7c6035401698a6c12360
Jan Jaap <janjaap@pagespeed.pro>
2017-07-21 17:16:36 +0200

    Update webworker-vs-execjs-ping-heavy.js

--- tests/webworker-vs-execjs-ping-heavy.js
+++ tests/webworker-vs-execjs-ping-heavy.js
@@ -1,5 +1,5 @@
 // heavy workload used by both exec.js and WebWorker
-var calculation = 'var result; for (var i=0; i<999999;i++){result = Math.pow(5,i);} for (var i = Math.pow(7, 10); i >= 0; i--) {result += Math.atan(i) * Math.tan(i);};';
+var calculation = 'var result=0; for (var i = 999999; i >= 0; i--) {result += Math.atan(i) * Math.tan(i);};';
 var PINGCODE = 'onmessage=function pong(){requestIdleCallback(function() {' + calculation + 'postMessage(result);});}';
 var PINGCODE_WEBWORKER = 'onmessage=function pong(){' + calculation + 'postMessage(result);}';
 
