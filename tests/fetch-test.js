7f37bb133264ee7668a06b5d20a62d7d00aabd75
optimalisatie <info@optimalisatie.nl>
2017-07-23 00:07:58 +0200

    new file:   tests/fetch-test.js 	new file:   tests/tests.html

---
+++ tests/fetch-test.js
@@ -0,0 +1,14 @@
+// normal fetch request
+var request = fetch('https://ajax.googleapis.com/ajax/libs/angularjs/1.6.4/angular.min.js')
+    .then(function(response) {
+        response.text().then(function(text) {
+            console.log('response', text.length);
+        });
+    }).catch(function(err) {
+        console.log('err', err.message);
+    });
+
+// abort request after 1s
+setTimeout(function() {
+    request.abort();
+}, 1000);
